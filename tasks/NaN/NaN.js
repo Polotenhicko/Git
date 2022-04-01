66d42f26081502d248f0b353d9db18c34b17cf9e
Polo <artem-gribanof@yandex.ru>
2022-04-01 19:06:48 +0300

    Создание структуры + отдельные задания в отдельную папку в tasks

---
+++ tasks/NaN/NaN.js
@@ -0,0 +1,31 @@
+// Нахождение NaN без Number.isNaN
+
+function checkNaN(values) {
+  for (const item of values) {
+    console.group();
+    console.log(`value: ${item}`);
+    if (+item !== +item) {
+      console.log('isNaN');
+    } else {
+      console.log('is Number');
+    }
+    console.groupEnd();
+  }
+}
+
+let values = [
+  1,
+  '1',
+  'Infinity',
+  'undefined',
+  'null',
+  'undefined',
+  null,
+  Infinity,
+  undefined,
+  NaN,
+  'NaN',
+  'asd',
+];
+
+checkNaN(values);
