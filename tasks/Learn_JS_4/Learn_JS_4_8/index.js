d0276d78719466c93aaa056458eefc6712cb1a8e
Polo <artem-gribanof@yandex.ru>
2022-04-04 00:38:29 +0300

    Добавление customer с преобразованием в примитивы

---
+++ tasks/Learn_JS_4/Learn_JS_4_8/index.js
@@ -0,0 +1,32 @@
+// Преобразование объектов в примитивы
+// Создание конструкта
+
+function Customer(name, purse) {
+  if (typeof name == 'string' && name) {
+    this.name = name;
+  } else {
+    this.name = 'Customer';
+  }
+
+  if (Number.isNaN(+purse)) {
+    this.purse = 0;
+  } else {
+    this.purse = purse;
+  }
+
+  this[Symbol.toPrimitive] = function (hint) {
+    switch (hint) {
+      case 'string':
+        return this.name;
+      case 'number':
+        return this.purse;
+      default:
+        return undefined;
+    }
+  };
+}
+
+let customer = new Customer('Custom', 100);
+
+console.log(`Имя покупателя ${customer}`);
+console.log(`Баланс: ${+customer}`);
