4d0eaff738a82be815be6864dad2fdd37aead854
Polo <artem-gribanof@yandex.ru>
2022-04-05 21:40:24 +0300

    Тему понял

--- tasks/Learn_JS_5/Learn_JS_5_1/index.js
+++ tasks/Learn_JS_5/Learn_JS_5_1/index.js
@@ -14,3 +14,57 @@ console.log(!!value); // true
 
 value = Number(0);
 console.log(!!value); // false
+
+text = String('aboba');
+console.log(!!text); // false
+
+// Добавление к примитивам новых свойств
+
+value.test = 22;
+value.doTest = function doIt() {
+  console.log('do it');
+};
+
+console.log(value.test); // undefined, в строгом режиме будет ошибка
+
+try {
+  value.doTest();
+} catch (er) {
+  // Ошибка
+  console.error(er);
+}
+
+text.test = 22;
+text.doTest = function doIt() {
+  console.log('do it');
+};
+
+console.log(text.test); // undefined, в строгом режиме будет ошибка
+try {
+  text.doTest();
+} catch (er) {
+  // Ошибка
+  console.error(er);
+}
+
+// Добавление новых свойств к примитиву, как к объекту
+
+let test = new String('test');
+
+test.toDo = function sayHello() {
+  console.log('hello!');
+};
+test.test = '123';
+
+test.toDo(); // 'hello!'
+console.log(test.test); // '123'
+
+test = new Number(24);
+
+test.toDo = function sayHello() {
+  console.log('hello!');
+};
+test.test = '123';
+
+test.toDo(); // 'hello!'
+console.log(test.test); // '123'
