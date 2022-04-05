5c153c4164cbe894a8f4deab5840010d8ffe4490
Polo <artem-gribanof@yandex.ru>
2022-04-05 18:44:43 +0300

    базовые свойства опциональной цепочки

--- tasks/Learn_JS_4/Learn_JS_4_6/index.js
+++ tasks/Learn_JS_4/Learn_JS_4_6/index.js
@@ -5,3 +5,22 @@ console.log(user.name); // undefined
 // console.log(user.name.firstName); // Error
 
 console.log(user.name?.firstName); // undefined
+
+try {
+  delete user12?.name; // удаляет user12.name если пользователь существует
+} catch (er) {
+  // ошибка ?
+  console.error(er);
+}
+
+user = null;
+
+console.log(user?.name); // undefined
+console.log(delete user?.name); // true
+
+try {
+  console.log(delete user.name); // true
+} catch (er) {
+  // ошибка, т.к. пытается удалить у undefined свойство name
+  console.error(er);
+}
