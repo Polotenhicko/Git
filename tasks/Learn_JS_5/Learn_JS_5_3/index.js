48b0244a31507fd499789060c6040ddf489d8041
Polo <artem-gribanof@yandex.ru>
2022-04-07 00:54:56 +0300

    Поиск внутри строк

--- tasks/Learn_JS_5/Learn_JS_5_3/index.js
+++ tasks/Learn_JS_5/Learn_JS_5_3/index.js
@@ -1,3 +1,4 @@
+'use strict';
 let str = 'aboba';
 console.log(str.length); // 5
 console.log(str[0]); // 'a'
@@ -6,3 +7,26 @@ console.log(`Символ на несуществующей позиции str[1
 console.log(
   `Символ на несуществующей позиции str.charAt(1000) = '${str.charAt(1000)}'`
 ); // ''
+
+try {
+  str[2] = 'G';
+} catch (er) {
+  // Ошибка в строгом режиме
+  console.error(er);
+}
+
+str = 'First text';
+console.log(str.indexOf('st')); // 3
+console.log(str.indexOf('first')); // -1
+console.log(str.indexOf('Fi', 2)); // -1
+
+str = 'customerUser';
+console.log(str.lastIndexOf('er')); // 10
+
+console.log(~2); // -(2+1) = -3
+
+console.log(str.includes('cust')); // true
+console.log(str.includes('cu', 1)); // false
+
+console.log(str.startsWith('cus')); // true
+console.log(str.endsWith('user')); // false
