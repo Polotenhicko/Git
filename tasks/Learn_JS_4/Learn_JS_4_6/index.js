9b20b226c4e490fe75a2d759336507c732a145c4
Polo <artem-gribanof@yandex.ru>
2022-04-07 16:10:58 +0300

    Добавил проверки для вычисляемых значений

--- tasks/Learn_JS_4/Learn_JS_4_6/index.js
+++ tasks/Learn_JS_4/Learn_JS_4_6/index.js
@@ -25,6 +25,8 @@ try {
   console.error(er);
 }
 
+// Вызов функции через опциональную цепочку
+
 user = {
   name: 'test',
   toDo() {
@@ -34,6 +36,12 @@ user = {
 
 user.toDo(); // test должен сделать: undefined
 
+user.toDoList = {
+  first: 'Первое задание',
+};
+
+user.toDo(); // test должен сделать: Первое задание
+
 delete user.toDo;
 
 user.toDo?.(); // Ничего не выведет
@@ -41,3 +49,32 @@ user.toDo?.(); // Ничего не выведет
 let user12 = null;
 
 console.log(user12?.toDo()); // undefined
+
+user.toDo = function toDo() {
+  return `${this.name} должен сделать: ${this.toDoList?.first}`;
+};
+
+// Опциональная цепочка с []
+
+let value = 'temp';
+
+console.log(user[value]); // undefined
+
+value = 'name';
+
+console.log(user[value]); // 'test'
+
+value = 'toDo';
+
+console.log(user[value]?.()); // 'test должен сделать: Первое задание'
+
+value = 'toCreate';
+
+console.log(user[value]?.()); // undefined
+
+try {
+  console.log(user[value]());
+} catch (er) {
+  // Ошибка, т.к. такой функции не существует
+  console.error(er);
+}
