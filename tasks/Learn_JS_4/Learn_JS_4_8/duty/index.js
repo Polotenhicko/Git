95989413d5e61ba96470444b2e8442f96b6e101b
Polo <artem-gribanof@yandex.ru>
2022-04-12 18:36:28 +0300

    Сделал проверку приведения объекта к примитиву без use strict

--- tasks/Learn_JS_4/Learn_JS_4_8/duty/index.js
+++ tasks/Learn_JS_4/Learn_JS_4_8/duty/index.js
@@ -119,3 +119,75 @@ objTest = {
 
 console.log(objTest + ' сложение со строкой'); // default valueOf
 console.log(objTest + 34); // default valueOf
+
+console.log('--------');
+console.log(
+  'Дополнение: Что будет если возвращать не примитив (без use strict):'
+);
+
+objTest = {
+  [Symbol.toPrimitive](hint) {
+    switch (hint) {
+      case 'string':
+        console.log('symbol string');
+        return obj;
+        break;
+      case 'number':
+        console.log('symbol number');
+        return obj;
+        break;
+      default:
+        console.log('symbol default');
+        return obj;
+        break;
+    }
+  },
+};
+
+try {
+  console.log(String(objTest));
+} catch (error) {
+  // Ошибка, нельзя конвертировать объект в примитив
+  console.error(error);
+}
+
+try {
+  console.log(Number(objTest));
+} catch (error) {
+  // Ошибка, нельзя конвертировать объект в примитив
+  console.error(error);
+}
+
+try {
+  console.log(objTest + 3);
+} catch (error) {
+  // Ошибка, нельзя конвертировать объект в примитив
+  console.error(error);
+}
+
+const objTest1 = {
+  name: 'Document',
+  number: 100,
+  toString() {
+    console.log('to string');
+    return obj;
+  },
+  valueOf() {
+    console.log('value of');
+    return obj;
+  },
+};
+
+try {
+  console.log(String(objTest1));
+} catch (error) {
+  // Ошибка. Вызываются 2 метода, toString и valueOf, а потом ошибка
+  console.error(error);
+}
+
+try {
+  console.log(Number(objTest1));
+} catch (error) {
+  // Ошибка. Вызываются 2 метода, toString и valueOf, а потом ошибка
+  console.error(error);
+}
