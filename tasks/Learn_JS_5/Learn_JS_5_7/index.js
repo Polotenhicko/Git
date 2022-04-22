09b049cab5939a565001fcaaccaaa0e2e5358c09
Polo <artem-gribanof@yandex.ru>
2022-04-22 20:03:07 +0300

    Доделал map

--- tasks/Learn_JS_5/Learn_JS_5_7/index.js
+++ tasks/Learn_JS_5/Learn_JS_5_7/index.js
@@ -15,3 +15,41 @@ map.set(NaN, 123);
 console.log(map.get(NaN)); // 123
 
 map.set('1', 'str1').set(1, 'num1').set(true, 'bool1');
+console.log('keys:');
+for (const iterator of map.keys()) {
+  console.log(iterator);
+}
+
+console.log('values:');
+for (const iterator of map.values()) {
+  console.log(iterator);
+}
+
+console.log('Пары:');
+for (const iterator of map) {
+  console.log(iterator);
+}
+
+console.log('Пары:');
+for (const iterator of map.entries()) {
+  console.log(iterator);
+}
+
+console.log('Имеет foreach');
+map.forEach((value, key) => {
+  console.log(`${key}: ${value}`);
+});
+
+const obj = {
+  name: 'John',
+  age: 30,
+};
+
+const map1 = new Map(Object.entries(obj));
+
+console.log(map1);
+
+console.log(Object.fromEntries(map1)); // Обратно в объект
+console.log('////////');
+//set
+console.log('set:');
