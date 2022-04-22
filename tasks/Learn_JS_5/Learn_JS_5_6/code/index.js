a3ee76fa3aa35d00f32654ed2e73a4091b98487c
Polo <artem-gribanof@yandex.ru>
2022-04-22 16:21:05 +0300

    Написал 1 часть задачи по итераторам

---
+++ tasks/Learn_JS_5/Learn_JS_5_6/code/index.js
@@ -0,0 +1,52 @@
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min + 1);
+}
+
+function iterator(obj) {
+  return {
+    current: random(obj.from, obj.to),
+    searchNumber: random(obj.from, obj.to),
+    from: obj.from,
+    to: obj.to,
+
+    next() {
+      console.log(`Ищем: ${this.searchNumber}`);
+      if (this.current !== this.searchNumber) {
+        return {
+          done: false,
+          value: (this.current = random(this.from, this.to)),
+        };
+      } else {
+        console.log('Нашли!');
+        return {
+          done: true,
+          value: this.current,
+        };
+      }
+    },
+  };
+}
+
+const obj = {
+  from: 0,
+  to: 500,
+
+  [Symbol.iterator]() {
+    return iterator(this);
+  },
+};
+
+for (const iterator of obj) {
+  console.log(iterator);
+}
+
+const obj2 = {
+  from: 100,
+  to: 200,
+};
+
+obj2[Symbol.iterator] = iterator(obj2);
+
+for (const iterator of obj2) {
+  console.log(iterator);
+}
