27148e601beef0f2f9e35fb6d76c04c65614f35b
Polo <artem-gribanof@yandex.ru>
2022-04-28 15:48:20 +0300

    Долг

--- tasks/Learn_JS_5/Learn_JS_5_9/duty/index.js
+++ tasks/Learn_JS_5/Learn_JS_5_9/duty/index.js
@@ -1,2 +1,14 @@
 // Массив является итерируемым объектом
+let arr = [1, 2, 3, 4];
+
+// У него есть символ итератор
+
+let iterator = arr[Symbol.iterator]();
+
+while (true) {
+  let result = iterator.next();
+  if (result.done) break;
+  console.log(result.value);
+}
+
 // Итерируемый объект не является массивом
