6da35c000715553f087b9a6f3bd7f5e7b40907c7
Polo <artem-gribanof@yandex.ru>
2022-04-28 18:58:44 +0300

    Доделал долг

--- tasks/Learn_JS_5/Learn_JS_5_9/duty/index.js
+++ tasks/Learn_JS_5/Learn_JS_5_9/duty/index.js
@@ -11,4 +11,34 @@ while (true) {
   console.log(result.value);
 }
 
+console.log(Array.isArray(arr)); // true
+
 // Итерируемый объект не является массивом
+
+const range = {
+  from: 1,
+  to: 5,
+
+  [Symbol.iterator]() {
+    this.current = this.from;
+    return this;
+  },
+
+  next() {
+    if (this.current <= this.to) {
+      return { done: false, value: this.current++ };
+    } else {
+      return { done: true };
+    }
+  },
+};
+
+iterator = range[Symbol.iterator]();
+
+while (true) {
+  let result = iterator.next();
+  if (result.done) break;
+  console.log(result.value); // 1 2 3 4 5
+}
+
+console.log(Array.isArray(range)); // false
