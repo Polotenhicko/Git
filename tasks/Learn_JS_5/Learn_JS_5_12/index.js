6d982d1baafb227970f1de37e8ff566ae6c6898d
Polo <artem-gribanof@yandex.ru>
2022-05-02 20:08:55 +0300

    Сделал теорию

--- tasks/Learn_JS_5/Learn_JS_5_12/index.js
+++ tasks/Learn_JS_5/Learn_JS_5_12/index.js
@@ -58,8 +58,90 @@ console.log(
 );
 console.log('/////////');
 console.log(
+  JSON.stringify(
+    meetup,
+    function replacer(key, value) {
+      console.log(key, value);
+      return key == 'occupiedBy' ? undefined : value;
+    },
+    2
+  )
+);
+
+room = {
+  name: '123',
+  number: 23,
+  toJSON() {
+    return this.name;
+  },
+  [Symbol.toPrimitive](hint) {
+    console.log(hint);
+    return hint;
+  },
+};
+
+meetup = {
+  title: 'Conference',
+  room,
+};
+// Нет преобразования в строку
+console.log(JSON.stringify(room)); // '123'
+
+console.log(JSON.stringify(meetup));
+
+json = JSON.stringify(meetup);
+
+console.log(JSON.parse(json)); // {title: 'Conference', room: '123'}
+
+let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+
+meetup = JSON.parse(str);
+
+console.log(meetup); // date - строка
+try {
+  console.log(meetup.date.getMonth()); // Ошибка
+} catch (er) {
+  console.error(er);
+}
+
+meetup = JSON.parse(str, function (key, value) {
+  return key == 'date' ? new Date(value) : value;
+});
+
+console.log(meetup.date.getMonth()); // 10
+
+/////////
+
+console.log('///////////////');
+
+room = {
+  number: 23,
+};
+
+meetup = {
+  title: 'Совещание',
+  occupiedBy: [{ name: 'Иванов' }, { name: 'Петров' }],
+  place: room,
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+console.log(
   JSON.stringify(meetup, function replacer(key, value) {
-    console.log(`${key}: ${value}`);
-    return key == 'occupiedBy' ? undefined : value;
+    if (value == meetup && key !== '') {
+      return undefined;
+    } else {
+      return value;
+    }
   })
 );
+
+/* в результате должно быть:
+{
+  "title":"Совещание",
+  "occupiedBy":[{"name":"Иванов"},{"name":"Петров"}],
+  "place":{"number":23}
+}
+*/
