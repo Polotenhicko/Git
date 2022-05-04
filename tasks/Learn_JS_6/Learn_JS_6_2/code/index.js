d3ab2fd2ffdf07ab9d1ac2af2a2619456c965a66
Polo <artem-gribanof@yandex.ru>
2022-05-04 20:25:58 +0300

    Сделал задачу

---
+++ tasks/Learn_JS_6/Learn_JS_6_2/code/index.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/572ab0cfa3af384df7000ff8
+
+// Задача поменять местами 2 элемента в массиве по индексам, которые указаны в массиве со 2 аргумента
+
+function shuffleIt(arr, ...swapArr) {
+  for (const [i, j] of swapArr) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  return arr;
+}
+
+console.log(shuffleIt([1, 2, 3, 4, 5], [1, 2])); // [1,3,2,4,5]
+console.log(shuffleIt([1, 2, 3, 4, 5], [1, 2], [3, 4])); // [1, 3, 2, 5, 4]
