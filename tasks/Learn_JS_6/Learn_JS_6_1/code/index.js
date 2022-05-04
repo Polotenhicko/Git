8fe12ba15daec4f7b0d16380f3fb31182695eda3
Polo <artem-gribanof@yandex.ru>
2022-05-04 16:31:13 +0300

    Сделал задачи

---
+++ tasks/Learn_JS_6/Learn_JS_6_1/code/index.js
@@ -0,0 +1,44 @@
+function sequenceSum(begin, end) {
+  if (end < begin) return NaN;
+
+  return end == begin ? begin : begin + sequenceSum(++begin, end);
+}
+
+console.log(sequenceSum(3, 2)); // NaN
+console.log(sequenceSum(1, 5)); // 15
+console.log(sequenceSum(4, 10)); // 49
+console.log(sequenceSum(-3, 2)); // -3
+console.log(sequenceSum(5, 5)); // 5
+
+console.log('/////////');
+
+function getStringCount(objOrArr) {
+  let stringCount = 0;
+  if (objOrArr)
+    for (const item of Object.values(objOrArr)) {
+      if (typeof item == 'object') {
+        stringCount += getStringCount(item);
+      } else if (typeof item == 'string') {
+        stringCount++;
+      }
+    }
+  return stringCount;
+}
+
+console.log(
+  getStringCount({
+    first: '123',
+    second: '123',
+    third: false,
+    fourth: ['anytime', 2, 3, '4'],
+    fifth: null,
+    obj: {
+      test: '123',
+    },
+    emptyArr: [],
+    emptyObj: {},
+    emptyStr: '',
+  })
+); // 6
+
+console.log(getStringCount(['1', '2', '3', ['4']])); // 4
