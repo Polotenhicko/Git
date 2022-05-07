8afb6b6b2c9a6192f5a0e48fa149b0ef259dea2a
Polo <artem-gribanof@yandex.ru>
2022-05-08 00:42:01 +0300

    Почти вся теория

--- tasks/Learn_JS_6/Learn_JS_6_3/index.js
+++ tasks/Learn_JS_6/Learn_JS_6_3/index.js
@@ -20,3 +20,43 @@ function sayHiBye(firstName, lastName) {
   console.log('Hello, ' + getFullName());
   console.log('Bye, ' + getFullName());
 }
+
+try {
+  console.log(getFullName());
+} catch (e) {
+  console.error(e); // Не нашёл во внутреннем окружении и во внешнем окружении
+}
+
+let test = 'test';
+
+if (true) {
+  let test2 = 'test2';
+  console.log(`${test2} ${test}`); // test2 test
+}
+
+try {
+  console.log(`${test}`);
+  console.log(`${test2}`); // is not defined
+} catch (e) {
+  console.error(e);
+}
+
+for (let i = 0; i < 10; i++) {
+  // У каждой итерации цикла своё собственное лексическое окружение
+  // {i: value}
+}
+
+try {
+  console.log(i); // Ошибка, нет такой переменной
+} catch (e) {
+  console.error(e);
+}
+
+{
+  // Создаётся собственное лексическое окружение
+  let message = 'Hello';
+
+  console.log(message); // Hello
+}
+
+console.log(message); // Ошибка: переменная message не определена
