b7b4a34c1920be2164eaf616c4264e32f7ce1225
Polo <artem-gribanof@yandex.ru>
2022-05-08 13:54:48 +0300

    Теория закончена

--- tasks/Learn_JS_6/Learn_JS_6_3/index.js
+++ tasks/Learn_JS_6/Learn_JS_6_3/index.js
@@ -59,4 +59,80 @@ try {
   console.log(message); // Hello
 }
 
-console.log(message); // Ошибка: переменная message не определена
+try {
+  console.log(message); // Ошибка: переменная message не определена
+} catch (e) {
+  console.error(e);
+}
+
+// IIFE
+
+// function() { // <-- Error: Unexpected token (
+
+//   let message = "Hello";
+
+//   alert(message); // Hello
+
+// }();
+
+// Нельзя сразу вызывать
+// function go() {
+
+// }();
+
+(function () {
+  console.log('go');
+})(); // 'go'
+
+console.log(function () {
+  console.log('go');
+}); // ƒ () {
+//   console.log('go');
+// }
+
+// Пути создания IIFE
+
+// prettier не даёт возможность так сделать
+// (function () {
+//   console.log('Скобки вокруг всего');
+// }());
+
+// !function () {
+//   console.log('Выражение начинается с логического оператора NOT');
+// }();
+
+// +function () {
+//   console.log('Выражение начинается с унарного плюса');
+// }();
+
+function f() {
+  let value = Math.random();
+
+  return function () {
+    alert(value);
+  };
+}
+
+// три функции в массиве, каждая из них ссылается на лексическое окружение
+// из соответствующего вызова f()
+let arr = [f(), f(), f()];
+
+// Нет ссылок на функции, а значит и лексическое окружение выше очищается
+arr.splice(0, 3);
+
+console.log(arr);
+
+let value = 'value';
+
+function f() {
+  let value = 'ближайшее значение';
+
+  function g() {
+    debugger; // в консоли value = 'value'
+  }
+
+  return g;
+}
+
+let g = f();
+g();
