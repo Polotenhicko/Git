fecc5230cd6813ab4a61e35f7368dbe38d35198f
Polo <artem-gribanof@yandex.ru>
2022-05-08 15:32:05 +0300

    Задачи по теории

--- tasks/Learn_JS_6/Learn_JS_6_3/index.js
+++ tasks/Learn_JS_6/Learn_JS_6_3/index.js
@@ -128,7 +128,7 @@ function f() {
   let value = 'ближайшее значение';
 
   function g() {
-    debugger; // в консоли value = 'value'
+    // debugger; // в консоли value = 'value'
   }
 
   return g;
@@ -136,3 +136,103 @@ function f() {
 
 let g = f();
 g();
+
+function makeCounter() {
+  let count = 0;
+
+  return function () {
+    return count++;
+  };
+}
+
+let counter11 = makeCounter();
+let counter22 = counter11;
+
+console.log(counter11()); // 0
+console.log(counter11()); // 1
+
+console.log(counter22()); // 2
+console.log(counter22()); // 3
+
+function sum(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+console.log('////////');
+console.log(sum(1)(2)); // 1+2 = 3
+console.log(sum(3)(2)); // 3+2 = 5
+
+function inBetween(a, b) {
+  return function (item) {
+    return item >= a && item <= b;
+  };
+}
+
+function inArray(arr) {
+  return function (item) {
+    return arr.includes(item);
+  };
+}
+
+console.log('////////');
+
+arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(arr.filter(inBetween(3, 6))); // 3,4,5,6
+console.log(arr.filter(inArray([1, 2, 10]))); // 1,2
+
+let users = [
+  { name: 'John', age: 20, surname: 'Johnson' },
+  { name: 'Pete', age: 18, surname: 'Peterson' },
+  { name: 'Ann', age: 19, surname: 'Hathaway' },
+];
+
+console.log('/////');
+
+function byField(type) {
+  return function (itemA, itemB) {
+    return itemA[type] > itemB[type] ? 1 : -1;
+  };
+}
+
+users.forEach((user) => console.log(user.name)); // John, Pete, Ann
+
+console.log(users.sort(byField('name')));
+users.forEach((user) => console.log(user.name)); // Ann, John, Pete
+console.log(users.sort(byField('age')));
+
+console.log('-------');
+
+function byField(field) {
+  return (a, b) => (a[field] > b[field] ? 1 : -1);
+}
+
+console.log(users.sort(byField('name')));
+users.forEach((user) => console.log(user.name)); // Ann, John, Pete
+
+console.log(users.sort(byField('age')));
+
+console.log('========');
+
+function makeArmy() {
+  let shooters = [];
+
+  let i = 0;
+  while (i < 10) {
+    let j = i;
+    let shooter = function () {
+      // функция shooter
+      console.log(j); // должна выводить порядковый номер
+    };
+    shooters.push(shooter);
+    i++;
+  }
+
+  return shooters;
+}
+
+let army = makeArmy();
+
+army[0](); // у 0-го стрелка будет номер 10
+// army[5]();
