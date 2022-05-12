b025660de89371dc4deec3924c6fea26517cd824
Polo <artem-gribanof@yandex.ru>
2022-05-12 23:57:02 +0300

    Доделал задачу

--- tasks/Learn_JS_6/Learn_JS_6_3/code/index_2.js
+++ tasks/Learn_JS_6/Learn_JS_6_3/code/index_2.js
@@ -1,53 +1,65 @@
 console.log('Задача Вариант №2');
 console.log('=====================');
 
+// Можно определить тип, за которым будет мониторинг, заранее
 function createCounter2(type = undefined) {
   let value = 0;
 
+  // По задаче новая сущность
   const CounterBaseValue = function CounterBaseValue() {
-    // Весь конструктор 2 задача
-    let value = 0;
+    // Весь конструктор 2 варианта
+
+    // Значение, на сколько изменяется при increment/decrement
+    let difference = 0;
+
+    // Возможные типы и действия при выводе
     this.types = new Map([
       [
         'increment',
         function () {
-          return value;
+          return difference;
         },
       ],
       [
         'decrement',
         function () {
-          return value ? -value : 0;
+          // Может быть 0, и тогда можно вывести -0, данный код это убирает
+          return difference ? -difference : 0;
         },
       ],
     ]);
 
+    // Изменить тип за которым идёт мониторинг
     this.changeType = function changeType(newType) {
       type = newType;
-      value = 0;
+      difference = 0;
     };
 
-    this.doValue = function doValue() {
-      if (this.types.has(type)) value++;
+    // Если такой тип есть, то счётчик увеличивается
+    this.doValue = function doValue(func) {
+      if (this.types.has(type) && func.name == type) difference++;
     };
 
+    // Получить значения, если такой тип есть в списке и выполнить функцию этого типа по выводу
     this.getValue = function getValue() {
-      if (this.types.has(type)) return this.types.get(type)();
-      return undefined;
+      return this.types.has(type) ? this.types.get(type)() : undefined;
     };
   };
 
   const Counter = function Counter() {
+    // Добавление новой сущности
     this.CounterBaseValue = new CounterBaseValue(); // 2 задача
 
     this.increment = function increment() {
       value++;
-      this.CounterBaseValue.doValue(); // 2 задача
+      // При действии мы вызываем метод сущности
+      this.CounterBaseValue.doValue(increment); // 2 задача
     };
 
     this.decrement = function decrement() {
       value--;
-      this.CounterBaseValue.doValue(); // 2 задача
+      // При действии мы вызываем сущность
+      this.CounterBaseValue.doValue(decrement); // 2 задача
     };
 
     this.getValue = function getValue() {
@@ -58,4 +70,44 @@ function createCounter2(type = undefined) {
   return new Counter();
 }
 
-// const count = createCounter();
+const count2 = createCounter2();
+count2.increment(); // 1
+count2.increment(); // 2
+count2.increment(); // 3
+count2.decrement(); // 2
+
+console.log(count2.getValue()); // 2
+
+console.log(count2.CounterBaseValue.getValue()); // undefined, т.к. тип не указан
+
+count2.CounterBaseValue.changeType('increment');
+console.log(count2.CounterBaseValue.getValue()); // 0
+
+count2.increment(); // 3
+count2.increment(); // 4
+count2.increment(); // 5
+count2.decrement(); // 4
+count2.decrement(); // 3
+
+console.log(count2.getValue()); // 3
+
+console.log(count2.CounterBaseValue.getValue()); // 3
+
+count2.CounterBaseValue.changeType('decrement');
+console.log(count2.CounterBaseValue.getValue()); // 0
+
+count2.increment(); // 4
+count2.decrement(); // 3
+count2.decrement(); // 2
+
+console.log(count2.getValue()); // 2
+console.log(count2.CounterBaseValue.getValue()); // 2
+
+count2.CounterBaseValue.changeType('aboba');
+console.log(count2.CounterBaseValue.getValue()); // undefined
+
+count2.increment(); // 3
+count2.decrement(); // 2
+
+console.log(count2.getValue()); // 2
+console.log(count2.CounterBaseValue.getValue()); // undefined
