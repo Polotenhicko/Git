44f017a67d69d1e51ccbf832f703c442aec9aa28
Polo <artem-gribanof@yandex.ru>
2022-05-12 11:45:28 +0300

    Сделал теорию

--- tasks/Learn_JS_6/Learn_JS_6_8/index.js
+++ tasks/Learn_JS_6/Learn_JS_6_8/index.js
@@ -12,3 +12,36 @@ let timerId = setTimeout('console.log(123)', 3e3);
 console.log('timerId:', timerId); // Выводит id таймера
 clearTimeout(timerId);
 console.log('timerId:', timerId); // Выводит тот же id таймера
+
+// Метод setInterval имеет такой же синтаксис
+
+timerId = setInterval(() => console.log('tick'), 2000);
+
+// Остановить через 5 сек
+
+setTimeout(() => {
+  clearInterval(timerId);
+  console.log('stop');
+}, 5000);
+
+timerId2 = setTimeout(function tick() {
+  console.log('tick timeout');
+  timerId2 = setTimeout(tick, 2000); // (*)
+}, 2000);
+
+setTimeout(() => console.log('0 задержка!'));
+
+console.log('Код весь выполнился!');
+
+clearTimeout(timerId2);
+
+function printNumbers(from, to) {
+  let value = from;
+  let interval = setInterval(function () {
+    console.log(value);
+    if (value == to) clearInterval(interval);
+    value++;
+  }, 1e3);
+}
+
+printNumbers(1, 3);
