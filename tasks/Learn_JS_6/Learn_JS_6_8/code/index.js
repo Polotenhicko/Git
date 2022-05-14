2bece2adc5ae010217f3d9482ca13421618c3e55
Polo <artem-gribanof@yandex.ru>
2022-05-14 17:31:54 +0300

    Начало секундомера есть

--- tasks/Learn_JS_6/Learn_JS_6_8/code/index.js
+++ tasks/Learn_JS_6/Learn_JS_6_8/code/index.js
@@ -8,3 +8,53 @@
 
 // Таймер должен быть "точным" - отсчитывать время с запуска через Date.now() или performance.now().
 // Обновляться должен достаточно часто, но не обязательно раз в миллисекунду(60 fps будет достаточно).
+
+function Stopwatch() {
+  let isPause = false;
+  let ms = 0;
+  let startDate;
+  let timeout;
+
+  function showTime(ms) {
+    const date = new Date(ms);
+    return {
+      hour: date.getUTCHours(),
+      min: date.getUTCMinutes(),
+      sec: date.getUTCSeconds(),
+      ms: date.getUTCMilliseconds(),
+    };
+  }
+
+  this.start = function start() {
+    isPause = false;
+    clearTimeout(timeout);
+    startDate = Date.now();
+
+    timeout = setTimeout(function timeoutFunc() {
+      ms = Date.now() - startDate;
+      timeout = setTimeout(timeoutFunc, 17);
+    }, 17);
+  };
+
+  this.pause = function pause() {
+    if (ms) {
+      isPause = true;
+      clearTimeout(timeout);
+      console.log(showTime(ms));
+    } else {
+      console.error('Секундомер не включён');
+    }
+  };
+
+  this.clear = function clear() {
+    if (ms && isPause) {
+      ms = 0;
+    } else {
+      console.error(
+        'Для очистки секундомер должен после запуска быть приостановлен'
+      );
+    }
+  };
+}
+
+const test = new Stopwatch();
