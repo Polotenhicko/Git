15e281c209b65b6db0cf5bc8404219bda12512fa
Polo <artem-gribanof@yandex.ru>
2022-06-05 21:08:21 +0300

    Теория по bind

---
+++ tasks/Learn_JS_6/Learn_JS_6_10/index.js
@@ -0,0 +1,53 @@
+const user = {
+  firstName: 'user',
+  logName() {
+    console.log(this.firstName);
+  },
+};
+
+setTimeout(user.logName, 500); // undefined, т.к. this потерялся
+
+// Можно решить это при помощи функции-обёртки
+// Но всё может сломать если перезаписать user
+
+setTimeout(() => {
+  try {
+    user.logName();
+  } catch (error) {
+    console.error(error); // user.logName is not a function
+  }
+}, 500);
+
+user.logName = 123;
+
+function logName() {
+  console.log(this.firstName);
+}
+
+// Можно забиндить контекст
+const bindFunc = logName.bind(user);
+
+bindFunc(); // 'user'
+
+// Даже сразу при создании
+const test = function (phrase) {
+  console.log(phrase, this.firstName);
+}.bind(user);
+
+// аргументы тоже передаются
+test('Фраза:'); // 'Фраза: user'
+
+// Теперь с timeout
+
+user.logNameTest = function () {
+  console.log(this.firstName);
+};
+
+// Работает
+setTimeout(user.logNameTest.bind(user), 1e3);
+
+// Для стрелочных всё по прежнему
+user.test = () => console.log(this);
+
+let aboba = user.test.bind(user);
+aboba();
