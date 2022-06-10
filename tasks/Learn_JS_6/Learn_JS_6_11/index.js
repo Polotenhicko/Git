ed2cf53d024b4bd76892ec0591e01eef32d13438
Polo <artem-gribanof@yandex.ru>
2022-06-10 13:05:07 +0300

    Проделал тесты со стрелочной, немного прифигел от увиденного

--- tasks/Learn_JS_6/Learn_JS_6_11/index.js
+++ tasks/Learn_JS_6/Learn_JS_6_11/index.js
@@ -79,3 +79,75 @@ function argFunc() {
 argArrowFunc(1, 2); // arguments is not defined
 
 argFunc(1, 2); // arguments есть
+
+console.log('//////////');
+
+const obj = {
+  test: 123,
+  methodArrow: () => {
+    console.log(this.test);
+  },
+  method: function () {
+    console.log(this.test);
+    const arrow = () => {
+      console.log(this.test);
+    };
+    arrow();
+  },
+};
+
+obj.methodArrow(); // Ошибка при use strict, а так = undefined
+obj.method(); // 123, 123
+
+console.log('--------');
+
+let testArrow;
+
+const objTest = {
+  test: 321,
+  method() {
+    testArrow = () => console.log(this.test);
+  },
+};
+
+function Beer() {
+  this.test = 123;
+  this.arrow = () => {
+    console.log(this.test);
+  };
+  this.method = function () {
+    try {
+      console.log(this.test);
+    } catch (e) {
+      console.error(e);
+    }
+    try {
+      const meth = function () {
+        console.log(this.test);
+      };
+      meth();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+}
+
+const inst = new Beer();
+
+inst.arrow(); // 123
+inst.method(); // 123, ошибка
+
+console.log('"""""""""""""""');
+
+const test1 = inst.arrow;
+const test2 = inst.method;
+
+test1(); // 123 ???????
+test2(); // ошибка, ошибка
+
+objTest.method();
+
+testArrow(); // 321, ахуеть
+
+// Получается, переменная функция идёт по ссылке, а т.к. стрелочная функция
+// берёт this снаружи, то и this берётся от туда, куда ведёт ссылка
