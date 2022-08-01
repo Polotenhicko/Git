161395b2dedc219aaff8b49280fc89d768e3ed5f
Polo <artem-gribanof@yandex.ru>
2022-08-01 22:03:19 +0300

    сделал задачу

--- tasks/Learn_JS_9/Learn_JS_9_5/code/index.js
+++ tasks/Learn_JS_9/Learn_JS_9_5/code/index.js
@@ -1,8 +1,8 @@
 // написать метод для класса родителя с использованием Symbo.species
 
 class MyArr extends Array {
-  maxValue() {
-    return new this.constructor(Math.max(...this));
+  maxAndMinValue() {
+    return new this.constructor[Symbol.species](Math.max(...this), Math.min(...this));
   }
 }
 
@@ -10,6 +10,13 @@ class PowerMyArr extends MyArr {
   isEmpty() {
     return this.length === 0;
   }
+
+  static get [Symbol.species]() {
+    return MyArr;
+  }
 }
 
 let arr = new PowerMyArr(1, 2, 3);
+
+// MyArr
+console.log(arr.maxAndMinValue()); // 3, 1
