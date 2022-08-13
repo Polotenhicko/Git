b66023c6123b0a5469f7a35ea5fbe1aa480368d7
Polo <artem-gribanof@yandex.ru>
2022-08-13 16:49:01 +0300

    сделал задачу по промису

--- tasks/Learn_JS_11/Learn_JS_11_2/index.js
+++ tasks/Learn_JS_11/Learn_JS_11_2/index.js
@@ -160,9 +160,31 @@ function delay(ms) {
 let date = Date.now();
 delay(3000).then(() => console.log('выполнилось через 3 секунды', Date.now() - date));
 
-function showCircle() {}
+function showCircle(cx, cy, radius) {
+  let div = document.createElement('div');
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + 'px';
+  div.style.top = cy + 'px';
+  div.style.border = '1px solid black';
+  div.className = 'circle';
+  document.body.append(div);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+
+      div.addEventListener('transitionend', function handler() {
+        div.removeEventListener('transitionend', handler);
+        resolve(div);
+      });
+    }, 1e3);
+  });
+}
 
 showCircle(150, 150, 100).then((div) => {
+  // после исполнения промиса
   div.classList.add('message-ball');
   div.append('Hello, world!');
 });
