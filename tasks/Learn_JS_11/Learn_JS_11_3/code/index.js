e58276d4ef5f329db386d2f83e3bf7aacf603fcd
Polo <artem-gribanof@yandex.ru>
2022-08-18 00:16:16 +0300

    наработки по задаче

--- tasks/Learn_JS_11/Learn_JS_11_3/code/index.js
+++ tasks/Learn_JS_11/Learn_JS_11_3/code/index.js
@@ -1,21 +1,44 @@
 // Короче, у промиса нельзя просто так узнать статус (pending, fullfilled, rejected)
 // Сделай так, чтобы было можно узнать
 
-function showStatus(isResolved) {
-  new Promise((resolve, reject) => {
-    console.log(`status: pending`);
+let status = undefined;
 
-    setTimeout(() => {
-      isResolved ? resolve(1) : reject(0);
-    }, 1e3);
-  }).then(
-    () => {
-      console.log(`status: 'fulfilled'`);
-    },
-    () => {
-      console.log(`status: 'rejected'`);
+class MyPromise extends Promise {
+  constructor(func) {
+    status = 'pending';
+    function wrapper(resultFunc, errorFunc) {
+      func(
+        (...args) => {
+          status = 'fulfilled';
+          resultFunc(...args);
+        },
+        (...args) => {
+          status = 'rejected';
+          errorFunc(...args);
+        }
+      );
     }
-  );
+    super(wrapper);
+  }
 }
 
-showStatus(true);
+// new MyPromise((resolve, reject) => {
+//   console.log(status);
+//   resolve(123);
+// }).then((result) => {
+//   console.log(status);
+//   console.log(result);
+// });
+
+new MyPromise((resolve, reject) => {
+  reject(new Error(123));
+}).then(
+  (result) => {
+    console.log(status);
+    console.log(result);
+  },
+  (error) => {
+    console.log(status);
+    console.log(error);
+  }
+);
