1ca6415a6b1d79059b32f39445359bf2e63393df
Polo <artem-gribanof@yandex.ru>
2022-08-23 18:58:41 +0300

    сделал задачу на myRace

--- tasks/Learn_JS_11/Learn_JS_11_5/code/index.js
+++ tasks/Learn_JS_11/Learn_JS_11_5/code/index.js
@@ -1,46 +1,16 @@
 // сделай Promise.myRace / Promise.myAll / Promise.myAllSettled по 11.5 (можешь что-то одно на выбор взять, можешь все взять)
 
 Promise.myRace = function myRace(iterator) {
-  const result = {
-    isChanged: false,
-    isError: false,
-    value: undefined,
-  };
-
-  const changeResult = (value, isError = false) => {
-    if (!result.isChanged) {
-      result.value = value;
-      result.isChanged = true;
-      if (isError) result.isError = true;
-    }
-  };
-
   try {
-    if (iterator[Symbol.iterator]) {
-      return Promise.resolve()
-        .then(() => {
-          for (const promise of iterator) {
-            promise.then(
-              (value) => {
-                console.log('for');
-                changeResult(value);
-              },
-              (error) => {
-                changeResult(error, true)();
-              }
-            );
-          }
-        })
-        .then(() => {
-          if (result.isError) {
-            throw new Error(result.value);
-          } else {
-            return result.value;
-          }
-        });
-    } else {
-      throw new Error('Объект без Symbol.iterator');
-    }
+    return new Promise((resolve, reject) => {
+      if (iterator[Symbol.iterator]) {
+        for (const promise of iterator) {
+          promise.then(resolve, reject);
+        }
+      } else {
+        throw new Error('Объект без Symbol.iterator');
+      }
+    });
   } catch (e) {
     Promise.reject(e).catch(console.error);
   }
@@ -58,16 +28,22 @@ Promise.myRace([
   }),
 ]).then(console.log);
 
-// Promise.myRace([
-//   new Promise((resolve, reject) => {
-//     resolve(1);
-//   }),
-//   new Promise((resolve, reject) => {
-//     resolve(2);
-//   }),
-//   new Promise((resolve, reject) => {
-//     resolve(3);
-//   }),
-// ]).then(console.log);
+Promise.myRace([
+  new Promise((resolve, reject) => {
+    resolve(1);
+  }),
+  new Promise((resolve, reject) => {
+    resolve(2);
+  }),
+  new Promise((resolve, reject) => {
+    resolve(3);
+  }),
+]).then(console.log);
 
 console.log('sync');
+
+Promise.myRace([
+  new Promise((resolve, reject) => setTimeout(() => resolve(1), 2000)),
+  new Promise((resolve, reject) => setTimeout(() => reject(new Error('Ошибка!')), 1000)),
+  new Promise((resolve, reject) => setTimeout(() => resolve(3), 3000)),
+]).catch(console.log); // 1
