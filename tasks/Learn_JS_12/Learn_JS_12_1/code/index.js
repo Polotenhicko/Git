309bd06c383b73781368e3ec4e483b2770b3229d
Polo <artem-gribanof@yandex.ru>
2022-08-29 10:32:54 +0300

    сделал архитектуру

--- tasks/Learn_JS_12/Learn_JS_12_1/code/index.js
+++ tasks/Learn_JS_12/Learn_JS_12_1/code/index.js
@@ -1,2 +1,45 @@
 // У двигателя есть несколько этапов запуска.Каждый этап занимает какое - то время и этапов несколько.
 // Написать функцию, которая будет принимать в себя генератор и запускать каждый этап двигателя
+
+function* startEngine() {
+  yield function (resolve) {
+    setTimeout(() => {
+      console.log('Включаю питание');
+      resolve();
+    }, 1e3);
+  };
+
+  yield function (resolve) {
+    setTimeout(() => {
+      console.log('Прокачиваю топливную смесь по форсункам');
+      resolve();
+    }, 1000);
+  };
+
+  yield function (resolve) {
+    setTimeout(() => {
+      console.log('Доза горючего попала в камеру сгорания');
+      resolve();
+    }, 1e3);
+  };
+
+  yield function (resolve) {
+    setTimeout(() => {
+      console.log('Запускаю стартер');
+      resolve();
+    }, 1e3);
+  };
+
+  yield function (resolve) {
+    setTimeout(() => {
+      console.log('Двигатель запущен');
+      resolve();
+    }, 1e3);
+  };
+}
+
+function wrapper(generator) {
+  const startGen = generator();
+}
+
+wrapper(startEngine);
