772125d168322f66b485485a484e3ef9fd9b8cd2
Polo <artem-gribanof@yandex.ru>
2022-09-16 00:38:37 +0300

    наконец теория

--- tasks/Learn_JS_14/Learn_JS_14_1/index.js
+++ tasks/Learn_JS_14/Learn_JS_14_1/index.js
@@ -333,3 +333,376 @@ console.log(user.checkPass('qwerty123')); // true
 // такой себе прокси, могут быть путаницы при разных ситуациях
 // если мы проксируем несколько раз или передаём объект и т.п.
 // не везде стоит такой прокси использовать
+
+// ещё пример
+
+let range = {
+  start: 1,
+  end: 10,
+};
+
+// хочу чтобы in показывал что число входит в диапозон
+
+// has(target, property)
+
+range = new Proxy(range, {
+  has: (target, prop) => prop >= target.start && prop <= range.end,
+});
+
+console.log(2 in range); // true
+console.log(11 in range); // false
+
+// попробуем оборачивать функции
+// apply(target, thisArg, args)
+// thisArg - контекст this
+// args - список аргументов
+
+// поменяем delay
+
+let delay = function delay(f, ms) {
+  // возвращает обёртку, которая вызывает функцию f через таймаут
+  return function () {
+    // (*)
+    setTimeout(() => f.apply(this, arguments), ms);
+  };
+};
+
+// проблема что мы теряем свойства name, length при обёртывании
+
+function sayHi(user) {
+  console.log(`Hi, ${user}`);
+}
+
+console.log(sayHi.length); // 1
+
+sayHi = delay(sayHi, 1e3);
+
+console.log(sayHi.length); // 0 !!!!
+
+delay = function delay(f, ms) {
+  return new Proxy(f, {
+    // передаём функцию и это работает
+    apply(target, thisArg, args) {
+      setTimeout(() => target.apply(thisArg, args), ms);
+    },
+  });
+};
+
+sayHi = function (user) {
+  console.log(`Hi, ${user}`);
+};
+
+sayHi = delay(sayHi, 1e3);
+
+console.log(sayHi.length); // 1
+
+sayHi('John'); // Hi, John
+
+// Reflect - встроенный объект, упрощающий создания прокси
+// ура ебать, я дожил до него
+// ахуеть, 400 строк теории, впервые такое
+
+// [[Get]], [[Set]] и т.п. существуют только в спеке и нельзя к ним обратиться
+// Reflect позволяет обойти это
+
+// его методы - минимальные обёртки вокруг внутренних методов
+
+// примеры
+// операция           Выозов Reflect                    Внутренний метод
+// obj[prop]          Reflect.get(obj, prop)            [[Get]]
+// obj[prop] = value  Reflect.set(obj, prop, value)     [[Set]]
+// delete obj[prop]   Reflect.deleteProperty(obj, prop) [[Delete]]
+// new F(value)       Reflect.construct(F, value)       [[Construct]]
+
+user = {};
+
+// вызываем операцию [[Set]] как функцию Reflect.set
+// но важно другое
+Reflect.set(user, 'name', 'John');
+
+console.log(user.name); // 'John'
+
+// для каждого внутреннего метода, перехватываемого Proxy,
+// есть соотв метод в Reflect, который имеет то же имя и те же аргументы, что и у ловушки Proxy
+
+// поэтому мы можем использовать Reflect чтобы перенаправить операцию на исходный объект
+
+// пример с прозрачными get, set
+
+user = {
+  name: 'Василий',
+};
+
+user = new Proxy(user, {
+  get(target, prop, receiver) {
+    console.log(`GET ${prop}`);
+    // читает свойство объекта, а мы возвращаем
+    return Reflect.get(target, prop, receiver);
+  },
+  set(target, prop, val, receiver) {
+    console.log(`SET ${prop}`);
+    // Reflect.set записывает свойство и возвращает true при успехе
+    return Reflect.set(target, prop, val, receiver);
+  },
+});
+
+// т.е. если хотим перенаправить вызов на объект, то вызываем Reflect
+// большинство можно и без этого, но есть нюансы
+
+// рассмотрим пример, где видно что Reflect.get крутой
+// и зачем нужен receiver
+
+// допустим есть свойство _name и геттер для него
+
+user = {
+  _name: 'Гость',
+  get name() {
+    return this._name;
+  },
+};
+
+let userProxy = new Proxy(user, {
+  get(target, prop, receiver) {
+    return target[prop];
+  },
+});
+
+// всё прозрачно, вроде ок
+console.log(userProxy.name); // Гость
+
+// усложним пример
+
+// унаследуем от проксированного user объект admin
+
+let admin = {
+  __proto__: userProxy,
+  _name: 'Админ',
+};
+
+// понятное дело почему не то, что ожидаем
+console.log(admin.name); // 'Гость'
+
+// для таких случаев нужен receiver
+// в нём хранится ссылка на правильный контекст this, который нужно передать геттеру
+// просто так не понять что у нас геттер или просто свойство, поэтому передаём в Reflect
+
+userProxy = new Proxy(user, {
+  get(target, prop, receiver) {
+    // receiver = admin
+    return Reflect.get(target, prop, receiver);
+  },
+  // или короче
+  // get(target, prop, receiver) {
+  //   return Reflect.get(...arguments);
+  // },
+});
+
+admin = {
+  __proto__: userProxy,
+  _name: 'Админ',
+};
+
+console.log(admin.name); // 'Админ'
+
+// прокси - идеальное средство для настройки поведения объектов на самом низком уровне
+// но есть ограничения
+
+// многие встроенные объекты используют "внутренние слоты"
+// это как свойства, но только для внутреннего использования в самой спеке
+
+// например Map хранит элементы во внутреннем слоте [[MapData]]
+// встроенные методы обращаются к слотам напрямую, не через [[Get]], [[Set]]
+// таким образом, прокси не может перехватить это
+
+// и вот сам нюанс, так как эти обращения идут к внутреннему слоту, то проксированный метод объекта не заработает
+
+let map = new Map();
+
+proxy = new Proxy(map, {});
+
+try {
+  // не можем обратиться к внутреннему слоту
+  // .set пытается обратиться к this.[[MapData]], которого нет у proxy
+  proxy.set('лол', 'ошибка');
+} catch (e) {
+  // TypeError: Method Map.prototype.set called on incompatible receiver #<Map>
+  console.error(e);
+}
+
+// мы можем это исправить
+
+proxy = new Proxy(map, {
+  get(target, prop, receiver) {
+    const value = Reflect.get(...arguments);
+    return typeof value === 'function' ? value.bind(target) : value;
+  },
+});
+
+// теперь всё работает потому что мы привязали к оригинальному map
+// когда this.[[MapData]], то будет всё ок
+proxy.set('test', 1);
+console.log(proxy.get('test')); // 1
+
+// это не относится к Array
+// он не использует слоты, т.к. давно уже тусит в js
+
+// нечто похожее происходит с приватными полями классов
+
+class User {
+  #name = 'Гость';
+
+  getName() {
+    return this.#name;
+  }
+}
+
+user = new User();
+user = new Proxy(user, {});
+
+try {
+  // приватные поля реализуются при помощи внутренних слотов!!!! ахуеть
+  console.log(user.getName());
+} catch (e) {
+  // TypeError: Cannot read private member #name from an object whose class did not declare it
+  console.error(e);
+}
+
+user = new User();
+// решение такое же как и в пред, привязка контекста
+user = new Proxy(user, {
+  get(target, prop, receiver) {
+    const value = Reflect.get(...arguments);
+    return typeof value === 'function' ? value.bind(target) : value;
+  },
+});
+
+console.log(user.getName()); // 'Гость'
+
+// но остались недостатки, методу передаётся оригинальный объект
+// а он может быть передан куда-то ещё
+
+// отключаемые прокси - прокси, который может быть отключён вызовом специальной функции
+// допустим у нас есть какой-то ресурс и мы бы хотели закрыть к нему доступ в любой момент
+// можем использовать отключаемый прокси, без ловушек
+// такой прокси будет передавать все операции на проксируемый объект и мы сможем отключить его в любой момент
+
+// let {proxy, revoke} = Proxy.revocable(target, handler)
+
+let object = {
+  data: 'Важная инфа',
+};
+
+let { proxy: proxy2, revoke } = Proxy.revocable(object, {});
+
+console.log(proxy2.data); // 'Важные данные'
+
+// удаляем все внутренние ссылки на оригинальный объект из прокси
+revoke();
+console.log(proxy2); // Proxy {}
+console.log(object); // {data: 'Важная инфа'}
+
+try {
+  // а всё, удаляет ссылки на ориг объект из прокси
+  console.log(proxy2.data);
+} catch (error) {
+  // TypeError: Cannot perform 'get' on a proxy that has been revoked
+  console.error(error);
+}
+
+// можем хранить функцию revoke в WeakMap чтобы легко найти её по объекту прокси
+
+let revokes = new WeakMap();
+
+object = {
+  data: 'Важная инфа',
+};
+
+let { proxy: proxy3, revoke: revoke3 } = Proxy.revocable(object, {});
+
+revokes.set(proxy3, revoke3);
+
+// ...
+
+revoke3 = revokes.get(proxy3);
+
+revoke3();
+
+try {
+  console.log(proxy3.data);
+} catch (e) {
+  // TypeError: Cannot perform 'get' on a proxy that has been revoked
+  console.error(e);
+}
+
+// преимущество такого подхода в том, что мы не должны таскать функцию revoke3 повсюду
+// мы получаем её по необходимости из WeakMap revokes
+
+// Мы использовали WeakMap вместо Map, чтобы не блокировать сборку мусора.
+// Если прокси объект становится недостижимым(то есть на него больше нет ссылок),
+// то WeakMap позволяет сборщику мусора удалить его из памяти вместе с соответствующей функцией revoke,
+// которая в этом случае больше не нужна.
+
+// создать прокси, который генерирует ошибку при попытке прочитать несуществующее свойство
+
+function wrap(target) {
+  return new Proxy(target, {
+    get(target, prop, receiver) {
+      if (prop in target) {
+        return Reflect.get(...arguments);
+      } else {
+        throw new Error('Ошибка');
+      }
+    },
+  });
+}
+
+user = {
+  name: 'John',
+};
+
+user = wrap(user);
+
+console.log(user.name); // 'John'
+try {
+  console.log(user.age); // Ошибка
+} catch (e) {
+  console.error(e);
+}
+
+array = [1, 2, 3];
+
+array = new Proxy(array, {
+  get(target, prop, receiver) {
+    if (prop < 0) {
+      prop = target.length + +prop;
+    }
+    return Reflect.get(target, prop, receiver);
+  },
+});
+
+console.log(array[-1]);
+console.log(array[-2]);
+console.log(array.length); // 3
+
+// Создайте функцию makeObservable(target), которая делает объект «наблюдаемым», возвращая прокси.
+
+function makeObservable(target) {
+  target.observe = (callback) => {
+    callback();
+  };
+  return new Proxy(target, {
+    set(target, prop, receiver) {
+      return Reflect.set(...arguments);
+    },
+  });
+}
+
+user = {};
+
+user = makeObservable(user);
+
+user.observe((key, value) => {
+  console.log(`SET ${key} = ${value}`);
+});
+
+user.name = 'John'; // SET name = John
