e85cf735b9a1a7de0a38986121e4f0da4e6ffe9c
Polo <artem-gribanof@yandex.ru>
2023-01-06 22:45:49 +0300

    задачи

---
+++ task2/Learn_JS_2/Learn_JS_2_3/code/index.js
@@ -0,0 +1,24 @@
+// Создайте дерево, которое по клику на заголовок скрывает-показывает потомков:
+
+const ul = document.getElementById('tree');
+
+document.addEventListener('click', function (e) {
+  if (!e.target.closest('#tree')) return;
+  const list = e.target.querySelector('ul');
+  if (list) list.hidden = !list.hidden;
+});
+
+// Сделать таблицу сортируемой: при клике на элемент <th> строки таблицы должны сортироваться по соответствующему столбцу.
+const table = document.getElementById('grid');
+table.addEventListener('click', function (e) {
+  const elem = e.target;
+  if (!elem.dataset.type || elem.nodeName !== 'TH') return;
+  const countColumn = Array.from(elem.closest('tr').children).findIndex((tag) => tag == elem);
+  const type = elem.dataset.type;
+  const arr = Array.from(e.currentTarget.querySelectorAll(`tbody tr`));
+  arr.sort((trA, trB) => {
+    if (type == 'number') return +trA.children[countColumn].innerText - +trB.children[countColumn].innerText;
+    if (type == 'string') return trA.innerText.localeCompare(trB.innerText);
+  });
+  console.log(arr);
+});
