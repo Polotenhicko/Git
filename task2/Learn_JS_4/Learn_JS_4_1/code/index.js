3889dfa4b3a2beeedfbc06089b0ad11befefc7a5
Polo <artem-gribanof@yandex.ru>
2023-01-29 02:55:44 +0300

    задача

--- task2/Learn_JS_4/Learn_JS_4_1/code/index.js
+++ task2/Learn_JS_4/Learn_JS_4_1/code/index.js
@@ -1,1 +1,8 @@
-// Добавьте пункт к выпадающему списку
+// Выведите значение и текст выбранного пункта.
+// Добавьте пункт: <option value="classic">Классика</option>.
+// Сделайте его выбранным.
+
+const genres = document.getElementById('genres');
+
+const selectedOption = genres.options[genres.selectedIndex];
+console.log(selectedOption);
