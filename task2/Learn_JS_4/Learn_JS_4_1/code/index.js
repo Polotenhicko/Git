121ea4294e380ca82078abfda05b337513910aa4
Polo <artem-gribanof@yandex.ru>
2023-01-29 17:11:22 +0300

    сделал задачу

--- task2/Learn_JS_4/Learn_JS_4_1/code/index.js
+++ task2/Learn_JS_4/Learn_JS_4_1/code/index.js
@@ -5,4 +5,19 @@
 const genres = document.getElementById('genres');
 
 const selectedOption = genres.options[genres.selectedIndex];
-console.log(selectedOption);
+console.log(selectedOption.value, selectedOption.text); // blues Блюз
+
+const classicOption = new Option('Классика', 'classic');
+genres.append(classicOption);
+
+// Сделать выбранным:
+// 1
+classicOption.selected = true;
+
+console.log(genres.value); // 'Classic'
+
+// 2
+classicOption.selected = false;
+
+genres.selectedIndex = 2;
+console.log(genres.value); // 'Classic'
