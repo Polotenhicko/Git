5c1da4771b7a60f5f79d687aad9bd42edcd3c8e1
Polo <artem-gribanof@yandex.ru>
2023-01-31 00:46:33 +0300

    тестирование задачи

--- task2/Learn_JS_3/Learn_JS_3_5/code/index.js
+++ task2/Learn_JS_3/Learn_JS_3_5/code/index.js
@@ -3,26 +3,7 @@
 let countPointer = 0;
 const root = document.getElementById('root');
 
-const listFingers = [
-  {
-    pointerId: null,
-    x: 0,
-    y: 0,
-  },
-  {
-    pointerId: null,
-    x: 0,
-    y: 0,
-  },
-];
-const firstFinger = listFingers[0];
-const secondFinger = listFingers[1];
-
-function setCoords(e, finger) {
-  finger.pointerId = e.pointerId;
-  finger.x = e.clientX;
-  finger.y = e.clientY;
-}
+function setCoords(e, finger) {}
 
 function pointerMove(e) {
   root.innerText += ' Движение двумя пальцами';
@@ -30,16 +11,14 @@ function pointerMove(e) {
 
 document.body.addEventListener('pointerdown', function pointerDownPrimary(e) {
   countPointer += 1;
-  if (firstFinger.pointerId == null) setCoords(e, firstFinger);
   if (countPointer !== 2) return;
-  setCoords(e, secondFinger);
-  document.body.addEventListener('pointermove', pointerMove);
+  // document.body.addEventListener('pointermove', pointerMove);
+  pointerMove();
 });
 
 document.body.addEventListener('pointerup', function pointerUp(e) {
-  const currentFInger = listFingers.find((finger) => finger.pointerId === e.pointerId);
-  if (!currentFInger) return;
-  currentFInger.pointerId = null;
   countPointer -= 1;
-  document.body.removeEventListener('pointermove', pointerMove);
+  if (countPointer) return;
+  // document.body.removeEventListener('pointermove', pointerMove);
+  root.innerText = 'Убрано';
 });
